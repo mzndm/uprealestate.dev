b4d0eaa10a447144a3983dbe8b2662e1f9ad2d47
Luis Figo <ace.spades@gmail.com>
2017-07-17 00:31:58 +0300

    add media styles for mobile phones

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,13 @@
 $('.language').click(function () {
     $('.language__list').toggleClass('language__list--active');
 });
-$('.language__list-item').click(function () {
-    $('.language__list').removeClass('language__list--active');
-});
 
 $('.currency').click(function () {
     $('.currency__list').toggleClass('currency__list--active');
 });
-$('.currency__list-item').click(function () {
-    $('.currency__list').removeClass('currency__list--active');
+
+$('.menu__button').click(function () {
+    $('.menu').slideToggle();
+    $(this).toggleClass('menu__button--active')
 });
+
